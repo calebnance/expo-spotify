4a41e7150c4e827b79f57239c92b51aa967ced6d
Caleb Nance <calebnance@gmail.com>
2019-05-07 01:49:10 -0400

    more album images added

--- src/api/preloadImages.js
+++ src/api/preloadImages.js
@@ -5,11 +5,23 @@ const user = require('../assets/images/user.png');
 // albums (mock local)
 // /////////////////////////////////////////////////////////////////////////////
 const exRe = require('../assets/images/albums/ex-re.jpg');
-const swimming = require('../assets/images/albums/mac-miller-swimming.jpg');
+const dearMissLonelyhearts = require('../assets/images/albums/dear-miss-lonelyhearts.jpg');
+const extraMachine = require('../assets/images/albums/extraordinary-machine.jpg');
+const seaOfCowards = require('../assets/images/albums/sea-of-cowards.jpg');
+const swimming = require('../assets/images/albums/swimming.jpg');
+const theCreekDrank = require('../assets/images/albums/the-creek-drank-the-cradle.jpg');
+const theLegendOfMrRager = require('../assets/images/albums/the-legend-of-mr-rager.jpg');
+const wishYouWereHere = require('../assets/images/albums/wish-you-where-here.jpg');
 
 export default {
   user,
 
+  dearMissLonelyhearts,
+  extraMachine,
   exRe,
-  swimming
+  seaOfCowards,
+  swimming,
+  theCreekDrank,
+  theLegendOfMrRager,
+  wishYouWereHere
 };
