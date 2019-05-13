0b64b574d2ec17ac4660f9fdc208f390af9939a6
Caleb Nance <calebnance@gmail.com>
2019-05-13 09:31:20 -0400

    more album data

--- src/mockdata/albums/index.js
+++ src/mockdata/albums/index.js
@@ -1,5 +1,11 @@
+import exRe from './exRe';
 import extraordinaryMachine from './extraordinaryMachine';
+import manOnTheMoon2 from './manOnTheMoon2';
+import swimming from './swimming';
 
 export default {
-  'Extraordinary Machine': extraordinaryMachine
+  'Ex:Re': exRe,
+  'Extraordinary Machine': extraordinaryMachine,
+  'Man On The Moon II: The Legend of Mr. Rager': manOnTheMoon2,
+  Swimming: swimming
 };
