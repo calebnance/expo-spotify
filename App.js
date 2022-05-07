75834a7b8506913b73b4daf31436872198e38c3e
calebnance <calebnance@gmail.com>
2022-05-07 17:36:14 -0400

    expo-app-loading => expo-splash-screen

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { StatusBar } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import { func } from './src/constants';
 
-// root navigation stack
+// root stack navigation
 import RootStack from './src/navigation/RootStack';
 
 // app context state
@@ -12,17 +12,34 @@ import AppState from './src/context/AppState';
 const App = () => {
   const [isLoading, setIsLoading] = React.useState(true);
 
-  // pre-loading assets/fonts?
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+
+        // pre-load/cache assets: images, fonts, and videos
+        await func.loadAssetsAsync();
+      } catch (e) {
+        // console.warn(e);
+      } finally {
+        // loading is complete, show app
+        setIsLoading(false);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  React.useCallback(async () => {
+    if (isLoading === false) {
+      // loading is complete, hide Splash Screen and show app
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
   if (isLoading) {
-    return (
-      <AppLoading
-        onError={() => {
-          // console.warn
-        }}
-        onFinish={() => setIsLoading(false)}
-        startAsync={func.loadAssetsAsync}
-      />
-    );
+    return null;
   }
 
   return (
